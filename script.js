fcebf9fd7389e3597f505aff28270bca52190f75
orsasson12 <55879173+orsasson12@users.noreply.github.com>
2022-02-02 19:00:12 +0200

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// const resume = document.getElementById("resume")
+// const list = document.getElementById("list")
+
+// list.style.display = 'none'
+
+// resume.addEventListener('click' , ()=>{
+//     list.style.display = "flex"
+// })
+// resume.addEventListener('dblclick' , ()=>{
+//     list.style.display = "none"
+// })
+
+
+
+
+
+// // const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// // const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+// // toggleButton.addEventListener('click' , ()=>{
+// //     navbarLinks.classList.toggle('active')
+// // })
+
+
+
